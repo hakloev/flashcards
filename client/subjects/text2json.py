5c2fa72fa650ee51f76772b5a64b6a722a5e4639
Håkon Ødegård Løvdal <me@hakloev.no>
2016-06-01 19:47:58 +0200

    Adds python script to translate from txt to json

--- client/subjects/text2json.py
+++ client/subjects/text2json.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import json
+import os
 
-quest = 'naive bayes ass'
-string = r'$v_{MAP} = \underset{v_j \in V}{argmax} ~P(v_j) \prod_{i} P(a_j|v_j)$'
+_file = sys.argv[1]
 
-obj = { 'answer': quest, 'question': string }
+if not _file.endswith('.txt'):
+    _file = '%s.txt' % _file
 
-print(json.dumps(obj))
+location = os.path.join(os.path.dirname(__file__), _file)
+print(location)
+
+data = None
+with open(location) as f:
+    data = f.readlines()
+
+contents = {
+    'subject': data[0],
+    'code': data[1],
+    'questions': []
+}
+
+for line in data[2:]:
+    raw_content = line.rstrip().split(':=')
+    question = {
+        'question': raw_content[0],
+        'answer': raw_content[1]
+    }
+    contents['questions'].append(question)
+
+new_file = location.split('.')[0] + '.json'
+with open(new_file, 'w+') as f:
+    f.write(json.dumps(contents))
